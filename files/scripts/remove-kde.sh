00113a6f36e97748870561594868263f7b530a3a
Matheus Abegg <1006630+mpabegg@users.noreply.github.com>
2026-01-28 16:05:45 -0300

    Add script to remove KDE desktop packages
    This script removes the mandatory packages from the kde-desktop group and cleans up the DNF cache.

---
+++ files/scripts/remove-kde.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -ouex pipefail
+
+dnf5 remove -y xwaylandvideobridge
+
+# Gets the mandatory packages installed in kde-desktop and removes them.
+dnf5 group info kde-desktop | \
+    sed -n '/^Mandatory packages\s*:/,/^\(Default\|Optional\) packages\s*:/ {
+        /^\(Default\|Optional\) packages\s*:/q  # Quit if we hit Default/Optional header
+        s/^.*:[[:space:]]*//p
+    }' | \
+    xargs dnf5 remove -y
+
+dnf5 clean all && \
+rm -rf /var/cache/dnf/*
+
